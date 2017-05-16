e2c7e5950bd81b516a798a3eeb5d3a13aa653132
dev <dev@giovanni>
2017-05-16 10:31:36 +0200

    add files

---
+++ src/speaker/Main.java
@@ -0,0 +1,9 @@
+package speaker;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		new Selector().execute(args);
+	}
+
+}
---
+++ src/speaker/Selector.java
@@ -0,0 +1,23 @@
+package speaker;
+
+import static java.util.Arrays.*;
+import static org.apache.commons.io.FileUtils.*;
+
+import java.io.File;
+import java.util.List;
+
+public class Selector {
+	
+		private static final String SPEAKER_SET = "speakers.txt";
+
+		public void execute(String[] args) throws Exception {
+			
+			File file = new File(SPEAKER_SET);
+			List<String> speakers = readLines(file, "UTF-8");
+			List<String> notAvailable = asList(args);
+			Presentation presentation = new Presentation(speakers, notAvailable);
+			Speaker speaker = presentation.getSpeaker();
+			System.out.println(speaker.getName());
+			
+	}
+}
