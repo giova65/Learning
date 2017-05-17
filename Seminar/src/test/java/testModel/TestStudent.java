369798d5e2f9cfc95ae8d1ee84c3e503f043c1c9
giova65 <giova.mele@gmail.com>
2017-05-17 16:29:09 +0200

    added Student

---
+++ Seminar/src/test/java/testModel/TestStudent.java
@@ -0,0 +1,13 @@
+package testModel;
+import org.junit.Test;
+
+import model.Student;
+
+public class TestStudent {
+	
+	@Test
+	public void testGetName(){
+		Student student = new Student();
+		
+	}
+}
