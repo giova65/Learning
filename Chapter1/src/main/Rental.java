fbaaba8e18faf2c608ffa5bb64970d531c566257
giova65 <giova.mele@gmail.com>
2017-05-22 13:49:31 +0200

    start Chapter 1, next step refactorin

---
+++ Chapter1/src/main/Rental.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Rental {
+	
+	private final Movie _movie;
+	private final int _daysRented;
+	
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+	
+	public Movie getMovie(){
+		return _movie;
+	}
+	
+	public int getDaysRented(){
+		return _daysRented;		
+	}
+}
