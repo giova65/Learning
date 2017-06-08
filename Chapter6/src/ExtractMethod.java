92ca4056d5c5b547073f9e1b1547f12d1f571e99
giova <giova.mele@gmail.com>
2017-06-08 22:36:02 +0200

    continue refactoring exercise

--- Chapter6/src/ExtractMethod.java
+++ Chapter6/src/ExtractMethod.java
@@ -1,15 +1,50 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ExtractMethod{
 	
 	private final String _name = "Giovanni";
+	private final List<String> _orders = new ArrayList<String>();
 	
-	void printOwning(double amount){
-		printDetails(amount);
+//	void printOwning(double amount){
+//		printDetails(amount);
+//	}
+//
+//	private void printDetails(double amount) {
+//		System.out.println("name:" + _name);
+//		System.out.println("amount:" + amount);
+//	}
+	
+	void printOwing(){
+		
+		//print banner
+		printBanner();
+		
+		//print details
+		printDetails();
 	}
 
-	private void printDetails(double amount) {
-		System.out.println("name:" + _name);
-		System.out.println("amount:" + amount);
+	private void printBanner() {
+		System.out.println("xxxxx");
+		System.out.println("******");
+		System.out.println("xxxxx");
 	}
+
+	private double calculate() {
+		double outstanding = 0.0;
+		Iterator e = _orders.iterator();
+		while(e.hasNext()){
+			String each = (String)e.next();
+			outstanding += each.length();
+		}
+		return outstanding;
+	}
+
+private void printDetails() {
+	System.out.println("name: " + _name);
+	System.out.println("amount:" + calculate());
+}
 	
 	
 
